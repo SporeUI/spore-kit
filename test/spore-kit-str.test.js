b64705aeb4c7a24f1edbfcf4cb521967db905cfa
Tony.Liang <pillar0514@163.com>
2018-08-31 18:55:48 +0800

    提供代码覆盖率测试

--- test/spore-kit-str.test.js
+++ test/spore-kit-str.test.js
@@ -6,3 +6,11 @@ const expect = global.expect;
 test('bLength("中文cc").length is 6', () => {
 	expect($str.bLength('中文cc')).toBe(6);
 });
+
+test('bLength("").length is 0', () => {
+	expect($str.bLength('')).toBe(0);
+});
+
+test('bLength("cc").length is 2', () => {
+	expect($str.bLength('cc')).toBe(2);
+});
