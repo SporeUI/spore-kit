e1d8c162b11a257da60af641a321039686d72aca
Tony.Liang <pillar0514@163.com>
2018-09-19 15:39:37 +0800

    更新入口文件

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+exports.arr = require('./packages/arr');
 exports.cookie = require('./packages/cookie');
 exports.dom = require('./packages/dom');
+exports.fn = require('./packages/fn');
 exports.io = require('./packages/io');
 exports.num = require('./packages/num');
 exports.obj = require('./packages/obj');
