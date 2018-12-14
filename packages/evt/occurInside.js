6c46875fa32ddd2791ae96a69deae6a0b8055054
TabSpace <pillar0514@163.com>
2018-12-14 12:13:00 +0800

    occurInside 脱离对 jq 的依赖

--- packages/evt/occurInside.js
+++ packages/evt/occurInside.js
@@ -1,6 +1,5 @@
 /**
  * 判断事件是否发生在一个 Dom 元素内。
- * - require jQuery/Zepto
  * - 常用于判断点击事件发生在浮层外时关闭浮层。
  * @method occurInside
  * @param {Object} event 浏览器事件对象
@@ -8,28 +7,23 @@
  * @returns {Boolean} 事件是否发生在 node 内
  * @example
  * $('.layer').on('click', function(evt){
- * 	if(occurInside(evt, $(this).find('close'))){
+ * 	if(occurInside(evt, $(this).find('close').get(0))){
  * 		$(this).hide();
  * 	}
  * });
  */
 
 function occurInside(event, node) {
-	var $ = window.$ || window.Zepto || window.jQuery;
-	node = $(node);
-
-	if (node.length && event && event.target) {
-		var element = node[0];
+	if (node && event && event.target) {
 		var pos = event.target;
-		while (pos.parentNode) {
-			if (pos === element) {
+		while (pos) {
+			if (pos === node) {
 				return true;
 			} else {
 				pos = pos.parentNode;
 			}
 		}
 	}
-
 	return false;
 }
 
