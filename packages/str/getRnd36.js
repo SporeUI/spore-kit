2fc5d13ee38df5a0a5320516faa16e782c86a745
TabSpace <pillar0514@163.com>
2020-03-25 10:54:07 +0800

    添加随机字符串生成方法

---
+++ packages/str/getRnd36.js
@@ -0,0 +1,15 @@
+/**
+ * 获取36进制随机字符串
+ * @method getRnd36
+ * @param {Float} [rnd] 随机数，不传则生成一个随机数
+ * @return {String} 转成为36进制的字符串
+ * @example
+ * getRnd36(0.5810766832590446); // 'kx2pozz9rgf'
+ */
+
+function getRnd36 (rnd) {
+	rnd = rnd || Math.random();
+	return rnd.toString(36).replace(/^0./, '');
+}
+
+module.exports = getRnd36;
---
+++ packages/str/getTime36.js
@@ -0,0 +1,15 @@
+/**
+ * 获取36进制日期字符串
+ * @method getTime36
+ * @param {Date} [date] 符合规范的日期字符串或者数字，不传参数则使用当前客户端时间
+ * @return {String} 转成为36进制的字符串
+ * @example
+ * getTime36('2020'); // 'k4ujaio0'
+ */
+
+function getTime36 (date) {
+	date = date ? new Date(date) : new Date();
+	return date.getTime().toString(36);
+}
+
+module.exports = getTime36;
