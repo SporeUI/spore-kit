8551c7961113c402d2b90bd3088c27648413ccf1
tabliang <tabliang@tencent.com>
2021-10-11 15:37:04 +0800

    test: 添加 cookie 组件单元测试

--- test/unit/cookie.test.js
+++ test/unit/cookie.test.js
@@ -7,15 +7,38 @@ console.log(
 );
 
 describe('cookie.cookie', () => {
+	const Cookie = $cookie.cookie;
 	test('cookie 以 URI 编码形式存值', () => {
-		$cookie.cookie.set('name', '中文', {
+		Cookie.set('key1', 'a b', {
 			expires: 1
 		});
-		expect(document.cookie).toBe('name=%E4%B8%AD%E6%96%87');
+		expect(document.cookie).toBe('key1=a%20b');
+		Cookie.remove('key1');
 	});
 	test('cookie 以 URI 编码形式取值', () => {
-		document.cookie = 'name=%E5%AD%97%E7%AC%A6';
-		const val = $cookie.cookie.get('name');
-		expect(val).toBe('字符');
+		document.cookie = 'key2=a%20b';
+		const val = Cookie.get('key2');
+		expect(val).toBe('a b');
+		Cookie.remove('key2');
+	});
+});
+
+// 从测试中可以看到 js-cookie 默认已经实现了默认字符编码
+// 因此之后的版本可以移除这个工具方法
+// 当前仓库保留该方法以实现对旧代码兼容
+describe('cookie.origin', () => {
+	const Cookie = $cookie.origin;
+	test('cookie 默认形式存值', () => {
+		Cookie.set('key3', 'a b', {
+			expires: 1
+		});
+		expect(document.cookie).toBe('key3=a%20b');
+		Cookie.remove('key3');
+	});
+	test('cookie 默认形式取值', () => {
+		document.cookie = 'key4=a%20b';
+		const val = Cookie.get('key4');
+		expect(val).toBe('a b');
+		Cookie.remove('key4');
 	});
 });
