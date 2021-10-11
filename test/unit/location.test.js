edb7e62491eea9bb1f5ae3107a8988557f195b3f
tabliang <tabliang@tencent.com>
2021-10-11 16:44:59 +0800

    test: 完善 location 组件单元测试

--- test/unit/location.test.js
+++ test/unit/location.test.js
@@ -34,3 +34,56 @@ describe('location.parse', () => {
 		expect(loc.href).toBe('http://username:password@localhost:8080/profile?beijing=huanyingni#123');
 	});
 });
+
+describe('location.setQuery', () => {
+	test('没有 query 参数就不追加 query', () => {
+		expect($location.setQuery('localhost')).toBe('localhost');
+	});
+	test('正常覆盖 query', () => {
+		const url1 = $location.setQuery('localhost', { a: 1 });
+		expect(url1).toBe('localhost?a=1');
+
+		const url2 = $location.setQuery('localhost?a=1', { a: 2 });
+		expect(url2).toBe('localhost?a=2');
+
+		const url3 = $location.setQuery('localhost?a=1', {
+			a: ''
+		});
+		expect(url3).toBe('localhost?a=');
+
+		const url4 = $location.setQuery('localhost?a=1', {
+			b: 2
+		});
+		expect(url4).toBe('localhost?a=1&b=2');
+
+		const url5 = $location.setQuery('localhost?a=1&b=1', {
+			a: 2,
+			b: 3
+		});
+		expect(url5).toBe('localhost?a=2&b=3');
+	});
+	test('移除 query key', () => {
+		const url = $location.setQuery('localhost?a=1', {
+			a: null
+		});
+		expect(url).toBe('localhost');
+	});
+	test('对空地址也可以追加 query', () => {
+		const url = $location.setQuery('', { a: 1 });
+		expect(url).toBe('?a=1');
+	});
+	test('存在 hash 时，query 插入到中间', () => {
+		const url = $location.setQuery('localhost#a=1', {
+			a: 2,
+			b: 3
+		});
+		expect(url).toBe('localhost?a=2&b=3#a=1');
+	});
+	test('对空地址 hash 也可以追加 query', () => {
+		const url = $location.setQuery('#a=1', {
+			a: 2,
+			b: 3
+		});
+		expect(url).toBe('?a=2&b=3#a=1');
+	});
+});
