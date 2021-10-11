2be05af50a36aeece99736b77ea655acb7812890
tabliang <tabliang@tencent.com>
2021-10-11 15:10:01 +0800

    test: 添加 callApp 方法的测试

--- test/unit/app.test.js
+++ test/unit/app.test.js
@@ -1,4 +1,8 @@
 const $app = require('spore-kit-app');
+// const $mockUa = require('jest-useragent-mock');
+const $browser = require('spore-kit-env/browser');
+
+jest.mock('spore-kit-env/browser');
 
 console.log(
 	Object.keys($app).map(
@@ -7,19 +11,70 @@ console.log(
 );
 
 describe('app.callUp', () => {
-	test('contains([1, 2], 1) => true', done => {
+	test('默认用 iframe 插入方式唤起客户端，可以在指定时间后调用 fallback', done => {
+		jest.useFakeTimers();
 		const fallbackUrl = 'https://fallback-url';
 		let onFallbackCalled = false;
+		let onTimeoutCalled = false;
+		$browser.mockReturnValue({
+			node: true
+		});
 		$app.callUp({
-			startTime: Date.now(),
-			waiting: 50,
-			waitingLimit: 10,
 			protocol: 'scheme://',
 			fallbackUrl,
 			fallback(url) {
 				expect(url).toBe(fallbackUrl);
+				expect(onTimeoutCalled).toBe(true);
 				expect(onFallbackCalled).toBe(true);
 				done();
+			},
+			onTimeout() {
+				onTimeoutCalled = true;
+			},
+			onFallback() {
+				onFallbackCalled = true;
+			}
+		});
+		jest.runAllTimers();
+	});
+
+	test('chrome 环境执行 window.open 唤起客户端', done => {
+		jest.useRealTimers();
+		jest.setTimeout(100);
+		$browser.mockReturnValue({
+			chrome: true
+		});
+
+		const fallbackUrl = 'https://fallback-url';
+		let onFallbackCalled = false;
+		let onTimeoutCalled = false;
+		let windowOpenCalled = false;
+		let windowCloseCalled = false;
+
+		window.open = jest.fn().mockImplementation(() => {
+			windowOpenCalled = true;
+			return {
+				close() {
+					windowCloseCalled = true;
+				}
+			};
+		});
+
+		$app.callUp({
+			waiting: 30,
+			waitingLimit: 30,
+			protocol: 'scheme://',
+			fallbackUrl,
+			fallback(url) {
+				expect(url).toBe(fallbackUrl);
+				expect(onTimeoutCalled).toBe(true);
+				expect(onFallbackCalled).toBe(true);
+				expect(windowOpenCalled).toBe(true);
+				expect(windowCloseCalled).toBe(true);
+				done();
+			},
+			onTimeout() {
+				onTimeoutCalled = true;
 			},
 			onFallback() {
 				onFallbackCalled = true;
