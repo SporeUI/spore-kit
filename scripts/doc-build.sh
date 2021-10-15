fa83450744b1ce75e9c1c120fefb25ee22b5e8c8
tabliang <tabliang@tencent.com>
2021-10-15 18:06:57 +0800

    ci: 简化文档生成命令

--- scripts/doc-build.sh
+++ scripts/doc-build.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-path=$1
-files=$(ls $path)
-for filename in $files
-do
-	cmd="npx documentation build ./index.js -f md -o ./docs/docs.md --markdown-toc false --np";
-	$cmd
-done
+npx documentation build ./index.js -f md -o ./docs/docs.md --markdown-toc false --np
