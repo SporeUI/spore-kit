d817906c016852fe1012450f32862ee822c84b50
tabliang <tabliang@tencent.com>
2021-10-20 11:18:02 +0800

    test: 完善测试配置

--- test/unit/io.test.js
+++ test/unit/io.test.js
@@ -9,7 +9,31 @@ $console.log(
 );
 
 describe('io.ajax', () => {
-  test('io.ajax', () => {
-
+  test('普通请求', (done) => {
+    let errorCalled = false;
+    let successCalled = false;
+    $io.ajax({
+      url: 'http://127.0.0.1:8080/null',
+      data: {
+        n1: 'v1',
+        n2: 'v2',
+      },
+      error(xhr, status) {
+        expect(status).toBe('error');
+        expect(xhr.readyState).toBe(4);
+        errorCalled = true;
+      },
+      success() {
+        successCalled = true;
+      },
+      complete(xhr, status) {
+        expect(errorCalled).toBe(true);
+        expect(successCalled).toBe(false);
+        expect(xhr.readyState).toBe(4);
+        expect(xhr.status).toBe(0);
+        expect(status).toBe('error');
+        done();
+      },
+    });
   });
 });
