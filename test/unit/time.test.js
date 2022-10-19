6d71b2ec616cdb6cbcd471cc0f89ff3ad075b5a5
TabSpace <pillar0514@163.com>
2022-10-19 16:25:37 +0800

    test: 💍 完善单元测试，减少报错概率

--- test/unit/time.test.js
+++ test/unit/time.test.js
@@ -17,17 +17,17 @@ describe('time.parseUnit', () => {
 
 describe('time.countDown', () => {
   test('提供定时触发能力', (done) => {
-    const target = Date.now() + 150;
+    const target = Date.now() + 700;
     let step = 0;
     $time.countDown({
       target,
-      interval: 50,
+      interval: 200,
       onChange(delta) {
         expect(typeof delta).toBe('number');
         step += 1;
       },
       onStop(delta) {
-        expect(step).toBe(4);
+        expect(step).toBe(5);
         expect(delta).toBeLessThanOrEqual(0);
         setTimeout(() => {
           expect($time.countDown.allMonitors[10]).toBe(null);
@@ -38,11 +38,11 @@ describe('time.countDown', () => {
   });
 
   test('定时器可提前终止', (done) => {
-    const target = Date.now() + 250;
+    const target = Date.now() + 1100;
     let step = 0;
     const cd1 = $time.countDown({
       target,
-      interval: 50,
+      interval: 200,
       onChange(delta) {
         expect(typeof delta).toBe('number');
         step += 1;
@@ -55,16 +55,16 @@ describe('time.countDown', () => {
     });
     setTimeout(() => {
       cd1.stop();
-    }, 120);
+    }, 500);
   });
 
   test('销毁定时器，不会执行后续回调', (done) => {
-    const target = Date.now() + 250;
+    const target = Date.now() + 900;
     let step = 0;
     let stopCalled = false;
     const cd1 = $time.countDown({
       target,
-      interval: 50,
+      interval: 200,
       onChange(delta) {
         expect(typeof delta).toBe('number');
         step += 1;
@@ -80,21 +80,21 @@ describe('time.countDown', () => {
         expect(stopCalled).toBe(false);
         done();
       }, 50);
-    }, 120);
+    }, 500);
   });
 
   test('设置服务器时间可以校准定时器', (done) => {
-    const target = Date.now() + 150;
+    const target = Date.now() + 700;
     let step = 0;
     $time.countDown({
-      base: Date.now() + 60,
+      base: Date.now() + 250,
       target,
-      interval: 50,
+      interval: 200,
       onChange() {
         step += 1;
       },
       onStop() {
-        expect(step).toBe(3);
+        expect(step).toBe(4);
         done();
       },
     });
@@ -103,21 +103,21 @@ describe('time.countDown', () => {
   test('可重设目标时间', (done) => {
     let step = 0;
     const now = Date.now();
-    const target1 = now + 200;
-    const target2 = now + 400;
+    const target1 = now + 500;
+    const target2 = now + 900;
     const cd1 = $time.countDown({
       target: target1,
-      interval: 100,
+      interval: 200,
       onChange() {
         step += 1;
       },
       onStop() {
-        expect(step).toBe(5);
+        expect(step).toBe(6);
         done();
       },
     });
     setTimeout(() => {
       cd1.setTarget(target2);
-    }, 100);
+    }, 300);
   });
 });
