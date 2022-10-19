efc0c0c6a4c1c2b635f1d5d9f527fb061f1575f6
TabSpace <pillar0514@163.com>
2022-10-19 16:59:07 +0800

    fix: 🐛 更新 rgbToHsl 方法

--- packages/util/rgbToHsl.js
+++ packages/util/rgbToHsl.js
@@ -26,22 +26,19 @@ function rgbToHsl(r, g, b) {
   var l = (max + min) / 2;
 
   if (max === min) {
-    // achromatic
-    h = 0;
-    s = 0;
+    s = 0; // achromatic
+    h = s;
   } else {
     var d = max - min;
     s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-    if (max === r) {
-      h = (g - b) / d + (g < b ? 6 : 0);
-    } else if (max === g) {
-      h = (b - r) / d + 2;
-    } else {
-      h = (r - g) / d + 4;
+    switch (max) {
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+      default: break;
     }
     h /= 6;
   }
-
   return [h, s, l];
 }
 
